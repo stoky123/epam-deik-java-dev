2a2c6dcb8509b15d7f92752b84dd797460dc8424
Balazs Takacs <takibali56@gmail.com>
2021-11-27 11:28:03 +0100

    Getting started with the project, basic configurations

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,6 +1,11 @@
 package com.epam.training.ticketservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.epam.training.ticketservice")
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
